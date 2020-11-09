d3cf52774717d90c31d0f3126b5ccf8438f1bcda
Evan Fujita <evanfujita@gmail.com>
2020-11-09 05:17:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 def reverse_each_word(string)
   new_array = Array.new
-  string = string.to_a
-  string.each do |word|
+  string.join.each do |word|
     new_array << word.reverse
   end
-  new_array.to_s 
+  new_array.%w
 end
